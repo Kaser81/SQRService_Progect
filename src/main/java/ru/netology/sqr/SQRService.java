9e8a39204f403a799ba2265ddccecec642648c93
Kaser81 <s.karablinn@gmail.com>
2022-02-25 01:36:20 +0700

    Added tests

--- src/main/java/ru/netology/sqr/SQRService.java
+++ src/main/java/ru/netology/sqr/SQRService.java
@@ -5,14 +5,12 @@ public class SQRService {
     public int numberOfSquares(int startNumber, int endNumber) {
         int value = 0;
         for (int i = 10; i <= 99; i++) {
-            if (i * i >= startNumber) {
-                if (i * i <= endNumber) {
-                    value++;
-
-                }
-
+            if (i * i >= startNumber && i * i <= endNumber) {
+//                if (i * i <= endNumber) {
+                value++;
             }
         }
         return value;
     }
 }
+
--- src/test/java/ru/netology/sqr/SQRServiceTest.java
+++ src/test/java/ru/netology/sqr/SQRServiceTest.java
@@ -7,13 +7,85 @@ import static org.junit.jupiter.api.Assertions.*;
 class SQRServiceTest {
 
     @Test
-    void numberOfSquares() {
+    void shouldNormalRange() {
         SQRService service = new SQRService();
         int startNumber = 400;
         int endNumber = 700;
         int expected = 7;
 
-        int actual = service.numberOfSquares( startNumber, endNumber);
+        int actual = service.numberOfSquares(startNumber, endNumber);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldMinimumRange() {
+        SQRService service = new SQRService();
+        int startNumber = 9;
+        int endNumber = 98;
+        int expected = 0;
+
+        int actual = service.numberOfSquares(startNumber, endNumber);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldKnownRange() {
+        SQRService service = new SQRService();
+        int startNumber = 10;
+        int endNumber = 99;
+        int expected = 0;
+
+        int actual = service.numberOfSquares(startNumber, endNumber);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldOverKnownRange() {
+        SQRService service = new SQRService();
+        int startNumber = 11;
+        int endNumber = 100;
+        int expected = 1;
+
+        int actual = service.numberOfSquares(startNumber, endNumber);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldVeryLargeRange() {
+        SQRService service = new SQRService();
+        int startNumber = 200;
+        int endNumber = 1000000;
+        int expected = 85;
+
+        int actual = service.numberOfSquares(startNumber, endNumber);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldNegativeRange() {
+        SQRService service = new SQRService();
+        int startNumber = -200;
+        int endNumber = -300;
+        int expected = 0;
+
+        int actual = service.numberOfSquares(startNumber, endNumber);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldZeroRange() {
+        SQRService service = new SQRService();
+        int startNumber = 0;
+        int endNumber = 200;
+        int expected = 5;
+
+        int actual = service.numberOfSquares(startNumber, endNumber);
 
         assertEquals(expected, actual);
     }
